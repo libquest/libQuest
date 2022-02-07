0a3455fa6659d5c72ee95222bc5717e4e7123510
theMackabu <sajjfa@gmail.com>
2022-02-07 03:04:25 +0000

    fix typo

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,6 +1,5 @@
 import pkg from "../../package.json";
 
-// Variabili di ambiente
 export const isBrowser = typeof window !== "undefined";
 export const isProd = process.env.NODE_ENV === "production";
 export const isDev = !isProd;
